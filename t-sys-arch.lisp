f2bfa49f7506d4b61851e74a70cc1fbeb40f8415
taeyoungchoon <taeyoungchoon@gmail.com>
2015-05-29 18:28:45 +0900

    add todo about windows but with probe-file

--- t-sys-arch.lisp
+++ t-sys-arch.lisp
@@ -3,7 +3,12 @@
 ;;
 ;; written by taeyoungchoon
 ;;
+;; todo
+;; - on windows
+;; *** - Win32 error 2 (ERROR_FILE_NOT_FOUND): The system cannot find the file specified.
+;;
 
+;; probe-file first
 (with-open-stream (st (run-program "/bin/uname" :arguments '("-a") :output :stream))
   (let ((arch (read st)))
     (cond ((eq arch 'linux)
