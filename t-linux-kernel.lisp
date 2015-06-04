64a7e3cf7a9981b63fe1fc7e0b22f7a15c2bbaa6
taeyoungchoon <taeyoungchoon@gmail.com>
2015-06-04 22:43:10 +0900

    support curl

--- t-linux-kernel.lisp
+++ t-linux-kernel.lisp
@@ -1,4 +1,5 @@
-(with-open-stream (st (run-program "lynx" :arguments (list "-dump" "https://www.kernel.org/finger_banner") :output :stream))
+;; (with-open-stream (st (run-program "lynx" :arguments (list "-dump" "https://www.kernel.org/finger_banner") :output :stream))
+(with-open-stream (st (run-program "curl" :arguments (list "-s" "https://www.kernel.org/finger_banner") :output :stream))
   (loop
        (let ((line (read-line st nil)))
 	 (if line
