0b168f0327335f69b56cfc24badf4446219843d6
taeyoungchoon <taeyoungchoon@gmail.com>
2015-06-07 18:57:50 +0900

    regexp to clisp

--- t-sys-uptime.lisp
+++ t-sys-uptime.lisp
@@ -1,7 +1,18 @@
+(require "~/quicklisp/setup.lisp")
+(ql:quickload :cl-ppcre)
+;;(defpackage #:t-shell
+;;  (:use #:cl #:cl-ppcre))
+
+;;(in-package #:t-shell)
+;;(require 'cl-ppcre)
+;;(asdf:load-system 'cl-ppcre)
+
 (with-open-stream (st (run-program "uptime" :arguments nil :output :stream))
-  (let ((uptime (read-line st)))
-    (format t "~a~%" uptime)))
-
-
-
+  (let ((uptime (read-line st nil nil)))
+;;    (format t "~a~&" (car (split "\\s+" uptime)))))
+;;    (format t "~a~&" (car (cl-ppcre:split "\\s+" uptime)))))
+    (format t "~a~&" (cl-ppcre:split "\\s+" uptime))))
+;;    (format t "~a~&" (car (cl-ppcre:split "\\s+" uptime)))))
+;;    (format t "~a~%" (type-of uptime))))
+;;    (format t "~a~%" uptime)))
 
