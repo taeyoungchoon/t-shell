3add8c27e9ec9b5c9159329662bb33f4190e7667
taeyoungchoon <taeyoungchoon@gmail.com>
2015-06-10 00:08:26 +0900

    todo touch?

--- t-sys-uptime.lisp
+++ t-sys-uptime.lisp
@@ -1,3 +1,5 @@
+;; 19:02  up  1:24, 3 users, load averages: 1.14 1.31 1.41
+
 (require "~/quicklisp/setup.lisp")
 (ql:quickload :cl-ppcre)
 ;;(defpackage #:t-shell
@@ -9,9 +11,11 @@
 
 (with-open-stream (st (run-program "uptime" :arguments nil :output :stream))
   (let ((uptime (read-line st nil nil)))
+    (multiple-value-bind (time up) (cl-ppcre:split "\\s+" uptime)
+      (format t "~a~&" time))))
 ;;    (format t "~a~&" (car (split "\\s+" uptime)))))
 ;;    (format t "~a~&" (car (cl-ppcre:split "\\s+" uptime)))))
-    (format t "~a~&" (cl-ppcre:split "\\s+" uptime))))
+;;    (format t "~a~&" (cl-ppcre:split "\\s+" uptime))))
 ;;    (format t "~a~&" (car (cl-ppcre:split "\\s+" uptime)))))
 ;;    (format t "~a~%" (type-of uptime))))
 ;;    (format t "~a~%" uptime)))
