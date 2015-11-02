4d414d2f453f876bdfd5c3be1a4c6e52e1c9fe9c
taeyoungchoon <taeyoungchoon@gmail.com>
2015-11-02 17:58:06 +0900

    done

--- sql-server-detaching-database.lisp
+++ sql-server-detaching-database.lisp
@@ -1,7 +1,10 @@
 (defun build-dbname (mon day)
-  (format nil "M6_LOG_2013~2,'0d~2,'0d_000000" mon day))
+  (format nil "M6_LOG_2015~2,'0d~2,'0d_000000" mon day))
 
 (defun main ()
-  (loop for mon from 1 to 2 do
-       (loop for day from 3 to 4 do
-	    (format t "sp_detach_db ~a~&" (build-dbname mon day)))))
+  (loop for mon from 6 to 7 do
+       (loop for day from 1 to 31 do
+	    (format t "sp_detach_db ~a~&" (build-dbname mon day))
+	    (format t "go~&"))))
+
+(main)
