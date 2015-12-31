6b48e88c4dbd35043ad0d40bf601e6ddbf68520c
taeyoungchoon <taeyoungchoon@gmail.com>
2015-12-31 16:52:32 +0900

    done

--- t-sys-hosts.lisp
+++ t-sys-hosts.lisp
@@ -1,15 +1,15 @@
-(require "~/quicklisp/setup.lisp")
-;;(require "t:/home/quicklisp/setup.lisp")
+;;(require "~/quicklisp/setup.lisp")
+(require "t:/home/quicklisp/setup.lisp")
 (ql:quickload :cl-ppcre)
 
 (defpackage :net.t0spring.t-shell
   (:use :common-lisp :cl-ppcre))
 (in-package :net.t0spring.t-shell)
 
-;;(with-open-file (stream "c:/Windows/System32/Drivers/etc/hosts")
-(with-open-file (stream "/etc/hosts")
+(with-open-file (st "c:/Windows/System32/Drivers/etc/hosts")
+;;(with-open-file (st "/etc/hosts")
   (loop 
-     (let ((line (read-line stream nil)))
+     (let ((line (read-line st nil)))
        ;; (if line
        ;; (unless (cl-ppcre:scan-to-strings "^#" line)
        (unless (cl-ppcre:scan "^#" line)
