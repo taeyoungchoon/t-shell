d4179e4ac0156a83870a2977a5cf41cf9542e757
taeyoungchoon <taeyoungchoon@gmail.com>
2016-05-22 22:36:41 +0900

    done

---
+++ macosx/df.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+
+my $line;
+my ($filesystem, $total, $used, $available, $capacity, $mount);
+my $left;
+my $condition;
+
+my $stamp = `date +%Y%m%d.%H%M%S`;
+$stamp =~ s/\r//g;
+$stamp =~ s/\n//g;
+
+foreach $line (`df -Pk -l`) {
+    if ($line !~ /^Filesystem/) {
+	# print $line;
+
+	($filesystem, $total, $used, $available, $capacity, $mount) = split(/\s+/,$line);
+	$left = int($available / $total * 100);
+
+	if (length($available) > 6) {
+	    $available = int($available/1024/1024);
+	    $available = $available . "g";
+	} else {
+	    $available = int($available/1024);
+	    $available = $available . "m";
+	}
+	
+	if ($left < 10) {
+	    $condition = "warn";
+	    printf("%s %s %d%s (%s) left\n",$stamp, $mount, $left, "%", $available);
+	}
+    }
+    
+}
