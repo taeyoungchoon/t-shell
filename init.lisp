9321da0347d159ce2665087cae7c45aebc2c46bc
taeyoungchoon <taeyoungchoon@gmail.com>
2016-07-27 22:59:33 +0900

    done

---
+++ init.lisp
@@ -0,0 +1,7 @@
+(defstruct being name age group)
+
+(make-being :name "taeyoungchoon" :age 41 :group "family")
+(make-being :name "yctae" :age 41 :group "work")
+(make-being :name "dolcom95" :age 41 :group "school")
+(make-being :name "t0spring" :age nil :group nil)
+
