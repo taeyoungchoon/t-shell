d94623e608104819e73832367acc085b5f3f6686
taeyoungchoon <taeyoungchoon@gmail.com>
2016-09-13 23:49:40 +0900

    done

--- github-clone.sh
+++ github-clone.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/taeyoungchoon/t-shell.git
 cd t-shell
-git config user.name "taeyoungchoon"
-git config user.email "taeyoungchoon@gmail.com"
+git config user.name ""
+git config user.email ""
 git config --global push.default simple
 git config --global credential.helper store
