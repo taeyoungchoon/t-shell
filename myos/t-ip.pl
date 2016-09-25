22bfeda5107d4f92949608f4c45793eb2326324c
taeyoungchoon <taeyoungchoon@gmail.com>
2016-09-25 23:07:31 +0900

    done

--- myos/t-ip.pl
+++ myos/t-ip.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 
-$cmd = sprintf("ifconfig | grep inet | grep -v inet6 | grep -v 127 | awk '{print $2}'");
+$cmd = sprintf("ifconfig | grep inet | grep -v inet6 | grep -v 127 | awk '{print \$2}'");
 system($cmd);
