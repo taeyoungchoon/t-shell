338e0f18cd1fc9d544072f66d320ea79df122fbe
taeyoungchoon <taeyoungchoon@gmail.com>
2016-11-23 15:41:21 +0900

    done

---
+++ t-perl/cwd.pl
@@ -0,0 +1,20 @@
+foreach my $file (`ls`) {
+  print $file;
+  my @stat = stat($file);
+  print @stat[7];
+  # sprintf("size of $file, @stat[7]");
+}
+
+__END__
+
+my $name = "cwd.pl";
+
+open(FN, $name);
+while(<FN>){
+  print;
+}
+close(FN);
+
+my @stat = stat($name);
+print "@stat[7]";
+
