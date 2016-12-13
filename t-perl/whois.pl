e15ada270a3ce71b1003431aab752766fbc4e3d4
taeyoungchoon <taeyoungchoon@gmail.com>
2016-12-13 11:42:09 +0900

    done

--- t-perl/whois.pl
+++ t-perl/whois.pl
@@ -3,7 +3,7 @@ use warnings;
 use Net::Whois::Raw;
 
 die "Usage: perl whois.pl <IP Address>" unless $ARGV[0];
-foreach ( split( /\n/ , whois(shift) ) {
+foreach ( split( /\n/ , whois(shift) ) ) {
   print $_, "\n" if ( m/^(netrange|orgname)/i );
 }
 	  
