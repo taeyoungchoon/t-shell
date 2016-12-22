ca1a841805699983560e1b0832d65665d513b8f8
taeyoungchoon <taeyoungchoon@gmail.com>
2016-12-22 14:24:45 +0900

    done

--- t-perl/whois.pl
+++ t-perl/whois.pl
@@ -4,6 +4,7 @@ use Net::Whois::Raw;
 
 die "Usage: perl whois.pl <IP Address>" unless $ARGV[0];
 foreach ( split( /\n/ , whois(shift) ) ) {
-  print $_, "\n" if ( m/^(netrange|orgname)/i );
+    # print $_, "\n" if ( m/^(netrange|orgname)/i );
+    print $_, "\n";
 }
 	  
