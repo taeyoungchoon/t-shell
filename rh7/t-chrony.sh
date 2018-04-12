f6aba2166bc31a54415ed0d47a46ab2ace02e94f
taeyoungchoon <taeyoungchoon@gmail.com>
2018-04-12 19:41:25 +0900

    d

--- rh7/t-chrony.sh
+++ rh7/t-chrony.sh
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-yum install chrony -y
-systemctl is-enabled chronyd
-systemctl enable chronyd
-systemctl is-active chronyd
-systemctl start chronyd
-timedatectl
-=======
 rpm -qi chrony >/dev/null 2>/dev/null
 if test $? -ne 0; then
 	yum install chrony -y
@@ -33,4 +25,3 @@ if [ $(which timedatectl) ] && timedatectl >/dev/null 2>/dev/null; then
 	fi
 	timedatectl | grep Local
 fi
->>>>>>> 1634bef8cb0c20f7b2e5af076aacd6ba6cab953f
