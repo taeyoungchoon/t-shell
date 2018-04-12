d252a4a053eddc60d2983f03d4835fd96581ffd9
taeyoungchoon <taeyoungchoon@gmail.com>
2018-04-12 19:44:00 +0900

    d

--- rh7/samba.sh
+++ rh7/samba.sh
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-yum install -y samba samba-client
-yum install -y telnet nmap
-systemctl is-enabled smb nmb
-systemctl enable smb nmb
-systemctl is-active smb nmb
-systemctl start smb nmb
-firewall-cmd --permanent --add-service=samba
-firewall-cmd --reload
-=======
 rpm -qi samba >/dev/null 2>/dev/null
 if test $? -ne 0; then
 	yum install samba
@@ -30,4 +20,3 @@ yum install samba-client telnet nmap -y
 firewall-cmd --permanent --add-service=samba
 firewall-cmd --reload
 firewall-cmd --list-all
->>>>>>> 1634bef8cb0c20f7b2e5af076aacd6ba6cab953f
