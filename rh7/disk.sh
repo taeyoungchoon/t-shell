a50dd71448a7a4432419d9e14795fd35e7781f13
taeyoungchoon <taeyoungchoon@gmail.com>
2018-04-13 22:28:21 +0900

    do

--- rh7/disk.sh
+++ rh7/disk.sh
@@ -1,6 +1,22 @@
-parted /dev/sdb print
-parted /dev/sdb mklabel gpt
+# parted /dev/sdb print
+# parted /dev/sdb mklabel gpt
 parted /dev/sdb mkpart primary ext4 1MiB 100MiB
 parted /dev/sdb mkpart primary ext4 100MiB 300MiB
 partprobe
-part /dev/sdb print
+# parted /dev/sdb print
+mkfs.ext4 /dev/sdb1
+mkfs.xfs -f /dev/sdb2
+[ ! -e /1 ] && mkdir /1
+[ ! -e /2 ] && mkdir /2
+mount /dev/sdb1 /1
+mount /dev/sdb2 /2
+df -Th
+lsblk -f
+<<COMMENT
+umount /1
+umount /2
+[ -e /1 ] && rmdir /1
+[ -e /2 ] && rmdir /2
+parted /dev/sdb print | grep primary | awk '{ print $1 }' | sed 's!^!parted /dev/sdb rm !' | sh
+partprobe
+COMMENT
