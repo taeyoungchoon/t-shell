70331a31fc69a98c66ba3f3a131587f1bf53973a
taeyoungchoon <taeyoungchoon@gmail.com>
2018-05-21 15:35:37 +0000

    d

--- rh7/t-forward.sh
+++ rh7/t-forward.sh
@@ -1,13 +1,12 @@
-firewall-cmd --permanent --add-rich-rule='rule family=ipv4 source address=192.168.33.200 forward-port port=8080 protocol=tcp to-port=80'
-firewall-cmd --reload
-firewall-cmd --list-all
-<<COMMENT
-route -n
-ip route
-ip addr
-firewall-cmd --add-rich-rule='rule family=ipv4 forward-port port=8080 protocol=tcp to-port=80'
+echo "man firewalld.richlanguage | grep to-port:" 
+man firewalld.richlanguage | grep to-port | sed 's/^\s*//'
 
-# man firewalld.richlanguage | grep to-port
-           forward-port port="port value" protocol="tcp|udp" to-port="port value" to-addr="address"
-           rule family="ipv6" source address="1:2:3:4:6::" forward-port to-addr="1::2:3:4:7" to-port="4012" protocol="tcp" port="4011"
+echo
+echo "cli:"
+firewall-cmd --permanent --add-rich-rule='rule family=ipv4 source address=192.168.33.200 forward-port port=8080 protocol=tcp to-port=80' 2>/dev/null >/dev/null
+firewall-cmd --reload >/dev/null
+firewall-cmd --list-rich-rules
+
+<<COMMENT
+yum upgrade firewalld
 COMMENT
