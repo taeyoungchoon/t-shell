c83688ebb804d9903fddbb502fc81d552fa842e6
taeyoungchoon <taeyoungchoon@gmail.com>
2018-07-15 20:01:19 +0900

    d

--- openstack/list.sh
+++
@@ -1,6 +0,0 @@
-# openstack server list -c Networks -c Name -f value | awk '{ print $2 " " $1 }' | sed 's/ctlplane=//' | sort
-openstack server list -c Networks -c Name -c Status -f value | \
-awk '{ print $3 " " $2 " " $1 }' | \
-sed -e 's/ctlplane=//' -e 's/overcloud\-//' -e 's/\-0//' | \
-sed -e 's/cont/Cont/' | \
-sort
