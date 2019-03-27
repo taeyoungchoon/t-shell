bcef573be6fb87906a7632b96b5301ac82159134
taeyoungchoon <taeyoungchoon@gmail.com>
2019-03-27 13:39:25 +0900

    ready for devstack installation using stable/rocky

---
+++ openstack/as-root/pre.sh
@@ -0,0 +1,3 @@
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+sudo su - stack
---
+++ openstack/as-stack/clone.sh
@@ -0,0 +1,1 @@
+git clone https://git.openstack.org/openstack-dev/devstack -b stable/rocky
