26a3bd73eb7a4ef74d05a9d1c066262e9b3ac5be
taeyoungchoon <taeyoungchoon@gmail.com>
2019-03-27 15:20:15 +0900

    d

--- openstack/as-stack/go.sh
+++ openstack/as-stack/go.sh
@@ -1,4 +1,5 @@
-git clone https://git.openstack.org/openstack-dev/devstack -b stable/rocky
+#git clone https://git.openstack.org/openstack-dev/devstack -b stable/rocky
+git clone https://git.openstack.org/openstack-dev/devstack -b stable/pike
 cp local.conf devstack/
 cd devstack
 ./stack.sh
