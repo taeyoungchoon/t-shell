930a75315d832d280538012348d6e412be52acf7
taeyoungchoon <taeyoungchoon@gmail.com>
2019-06-23 00:35:02 +0900

    d

---
+++ rrun/rrun.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use feature qw/ say switch /;
+use Term::ANSIColor;
+
+print @ARGV;
+
+__END__
+build request directory
+makefile and all to do something as single command, make
+output with extension 'out'
+
+request directory as argment
+hosts
+target
+serviceping
+scp -r template to remote:/tmp/rrun-req-idx/
+ssh target cd then fire
+scp out to local directory with 'out'
+
+
