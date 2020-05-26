62149294db8e4b9dd47a5616a31aed9b39745fe5
taeyoungchoon <taeyoungchoon@gmail.com>
2020-05-26 21:09:40 +0900

    d

---
+++ conn-logos-repo/t-chrony.sh
@@ -0,0 +1,37 @@
+rpm -qi chrony >/dev/null 2>/dev/null
+if test $? -ne 0; then
+	yum install chrony -y
+fi
+
+systemctl is-enabled chronyd >/dev/null 2>/dev/null
+if test $? -ne 0; then
+	systemctl enable chronyd
+fi
+
+systemctl is-active chronyd >/dev/null 2>/dev/null
+if test $? -ne 0; then
+	systemctl start chronyd
+fi
+
+# if [ $(which timedatectl) ] && timedatectl >/dev/null 2>/dev/null; then
+# 	timedatectl | grep "NTP enabled"
+# fi
+
+# one more thing
+if [ $(which timedatectl) ] && timedatectl >/dev/null 2>/dev/null; then
+	timedatectl | grep KST >/dev/null 2>/dev/null
+	if test $? -ne 0; then
+		timedatectl set-timezone Asia/Seoul
+	fi
+fi
+
+sourcing=`chronyc sources | grep "\^\*" | wc -l`
+if test $sourcing -ne 1; then
+	systemctl restart chronyd
+	sleep 3
+fi
+
+gap=`chronyc tracking | grep ^System | awk '{ print $4 }' |  cut -d "." -f 1`
+if test $gap -ne 0; then
+	chronyc makestep
+fi
---
+++ conn-logos-repo/t-httpd.sh
@@ -0,0 +1,44 @@
+rpm -qi httpd >/dev/null 2>/dev/null
+if test $? -ne 0; then
+	yum install httpd -y
+fi
+
+systemctl is-enabled httpd >/dev/null 2>/dev/null
+if test $? -ne 0; then
+	systemctl enable httpd
+fi
+
+systemctl is-active httpd >/dev/null 2>/dev/null
+if test $? -ne 0; then
+	systemctl start httpd
+fi
+
+systemctl status httpd
+
+yum install mod_ssl mod_wsgi curl elinks telnet nmap -q -y
+
+#read -p "Press enter to continue"
+
+httpd -t
+httpd -t -D DUMP_VHOSTS
+
+ps -ef | grep httpd
+ps -eZ | grep httpd
+
+ss -lntp | grep httpd
+
+firewall-cmd --permanent --add-service=http --add-service=https
+firewall-cmd --reload
+firewall-cmd --list-all
+
+nmap localhost -p80,443
+
+# and more
+echo "server1.f.com" > /var/www/html/index.html
+mkdir /var/www/html/owndir
+echo "server1.f.com then sub" > /var/www/html/owndir/index.html
+
+mkdir -p /srv/vh1/www
+echo "vh1.f.com" > /srv/vh1/www/index.html
+
+cp myapp.wsgi /srv/vh1/www/
