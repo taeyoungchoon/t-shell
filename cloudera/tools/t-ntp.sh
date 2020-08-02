255d2b9f640c148b5276d425fdbc9819d640cb93
taeyoungchoon <taeyoungchoon@gmail.com>
2020-08-02 13:41:58 +0000

    d
    Former-commit-id: 42f247be75efe13c70d6d18ec08f4486bfedf10f
    Former-commit-id: e70ea9232340871afb8b28b7a642ecc28d08b68f

--- cloudera/tools/t-ntp.sh
+++ cloudera/tools/t-ntp.sh
@@ -1,1 +1,45 @@
-sudo yum install ntp -y
+# install
+
+function install() {
+    echo 'install called'
+    sudo yum install ntp -y
+}
+
+rpm -q ntp 1>/dev/null 2>/dev/null
+returncode=$?
+
+if [[ $returncode != 0 ]]; then
+    echo 'not installed'
+    echo 'call install'
+    install
+fi
+
+function active() {
+    echo 'active called'
+    sudo systemctl start ntpd
+}
+
+sudo systemctl is-active ntpd 1>/dev/null 2>/dev/null
+returncode=$?
+
+if [[ $returncode != 0 ]]; then
+    echo 'not active'
+    echo 'call active'
+    active
+fi
+
+function enable() {
+    echo 'enable called'
+    sudo systemctl enable ntpd
+}
+
+sudo systemctl is-enabled ntpd 1>/dev/null 2>/dev/null
+returncode=$?
+
+if [[ $returncode != 0 ]]; then
+    echo 'not enabled'
+    echo 'call enable'
+    enable
+fi
+
+
