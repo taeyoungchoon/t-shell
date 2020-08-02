3a9161f0033f06f2acd52fc3bef5c17deb161d89
taeyoungchoon <taeyoungchoon@gmail.com>
2020-08-02 13:42:25 +0000

    d
    Former-commit-id: afa1faff66b01d2beb93b37bd528e2623d3d139d
    Former-commit-id: cdc2edde6b25981ea4c8bc0663b1ae8824c25953

--- cloudera/t-deploy.sh
+++ cloudera/t-deploy.sh
@@ -1,1 +1,4 @@
-tar cvf - bin | ssh 172.20.99.100 "tar xvf -"
+# tar cvf - bin | ssh 172.20.99.120 "tar xvf -"
+cat t-deploy.sh | ssh 172.20.99.120 "cat > t-deploy.sh"
+#tar cvf - t-shell/cloudera/tools | ssh 172.20.99.120 "tar xvf -"
+#ln -s t-shell/cloudera/tools bin
