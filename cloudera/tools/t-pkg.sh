d4a372e7fe069e42368914dd20e366e913fec7f5
taeyoungchoon <taeyoungchoon@gmail.com>
2020-08-03 05:24:46 +0000

    d
    Former-commit-id: 8eb67ebceaa93d229dd98e32a5caa9954f359b79 [formerly f65d6b9eef89b714b3e08681e0e7e93ce4be5ccc] [formerly 12344f385045c62882e3ee2afea8bc4adc522d90]
    Former-commit-id: 8eb67ebceaa93d229dd98e32a5caa9954f359b79 [formerly f65d6b9eef89b714b3e08681e0e7e93ce4be5ccc]
    Former-commit-id: b2fb30931721137b893390e51ee6206e3481a243
    Former-commit-id: 1a1cf259a208317be4ef5cce72fc87765e23ff42

--- cloudera/tools/t-pkg.sh
+++ cloudera/tools/t-pkg.sh
@@ -1,3 +1,3 @@
-sudo yum install sshpass python-ethtool pyYAML -y
+sudo yum install sshpass python-ethtool PyYAML -y
 sudo yum install git emacs-nox tmux -y
 
--- cloudera/tools/t-selinux.sh
+++ cloudera/tools/t-selinux.sh
@@ -1,6 +1,15 @@
-if [[ "sestatus  | grep 'SELinux status' | grep disabled" ]]; then 
-    echo 'disabled'; 
-else
-    echo 'enabled';
+function disable() {
+    echo "disable called"
+    sudo sed -i "s/SELINUX=permissive/SELINUX=disabled/" /etc/selinux/config
+}
+
+sestatus  | grep 'SELinux status' | grep enabled
+returncode=$?
+
+if [[ $returncode == 0 ]]; then
+    echo 'enabled'
+    echo 'call disable'
+    disable
+    echo 'reboot required to be sure'
 fi
 
