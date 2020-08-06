3e756cf1e7f75fce1a0e480be08382003c8db33c
taeyoungchoon <taeyoungchoon@gmail.com>
2020-08-06 13:08:17 +0000

    d
    Former-commit-id: 6d4cbc26d0a409cf0e1239ddafad3fe2f1a6f009 [formerly d946557314fe7ee948dfaab6edd0e4eaefde74b6] [formerly ae51763e794144a26c1fbdd121ce0db2ea01276a]
    Former-commit-id: 6d4cbc26d0a409cf0e1239ddafad3fe2f1a6f009 [formerly d946557314fe7ee948dfaab6edd0e4eaefde74b6]
    Former-commit-id: f3e0d8cafab828a5f4dbcb56e08ebe589ce91e97
    Former-commit-id: 97a7a1de6545b5f9ce5b595ecdb081e0be4a3543

--- cloudera/java/install.sh
+++ cloudera/java/install.sh
@@ -1,6 +1,7 @@
 tar xvfz OpenJDK8U-jdk_x64_linux_8u232b09.tar.gz 
 sudo mv openjdk-8u232-b09 /usr/lib/jvm/
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/openjdk-8u232-b09/bin/java 1
-sudo update-alternatives --config java
+#sudo update-alternatives --config java
+sudo update-alternatives --set java /usr/lib/jvm/openjdk-8u232-b09/bi/java 1
 java -version
 
