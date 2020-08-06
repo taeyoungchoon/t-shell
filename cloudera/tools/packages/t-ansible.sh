f5b3e4a8fe27dd1c2e22f3fc94840f76fd12ebd1
taeyoungchoon <taeyoungchoon@gmail.com>
2020-08-06 13:34:11 +0000

    clean up rpms
    Former-commit-id: f27b734ed846c8c82eedfba2072184ae18c2af6a
    Former-commit-id: 4b230a43a5b9bd23f63c79810098d8c9681a72ef

--- cloudera/tools/packages/t-ansible.sh
+++ cloudera/tools/packages/t-ansible.sh
@@ -1,1 +1,7 @@
-sudo yum localinstall *.rpm -y
+add repo of ansible then do something
+yum install x.rpm --downloadonly
+cd /var/cache/x
+createrepo .
+yum-config-x --add-repo
+yum install x
+# sudo yum localinstall *.rpm -y
