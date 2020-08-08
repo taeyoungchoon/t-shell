44ac3ddc7ad6bfebcf951d59f10a154a498b6d13
taeyoungchoon <taeyoungchoon@gmail.com>
2020-08-08 17:03:00 +0900

    d
    Former-commit-id: 21d6d2048a8258217208a6d2bb433ac6a08a0797
    Former-commit-id: 70295b77929df51f7814ddcf772c29123fc20ebc

---
+++ sshpass/cloudera.sh
@@ -0,0 +1,6 @@
+cat cloudera.sh 
+for ip in 62 63 64 82 83 84 85 86 87 91 92 93 101 102
+do
+    sshpass -p '123$qwer' ssh -o "StrictHostKeyChecking=no" 112.$ip "echo -start--;hostname; $1"
+    # sshpass -p '123$qwer' ssh -o "StrictHostKeyChecking=no" 112.41 "curl http://172.29.112.71/ansible/install.sh | sh; hostname"
+done
