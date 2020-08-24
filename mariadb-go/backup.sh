b0b0450acced37ca0d3486cdb3c9a1ccfae25144
taeyoungchoon <taeyoungchoon@gmail.com>
2020-08-24 13:03:26 +0000

    much better but exception
    Former-commit-id: 46df04a6793a30fea0d47c0d379f35a810bee82b
    Former-commit-id: c9cc626063a73f9967ed5639185ae7b27ea39896

--- mariadb-go/backup.sh
+++ mariadb-go/backup.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 ### ENV
-BASEDIR=/usr/local/mariadb-10.2.32
-BASEDIRALIAS=/usr/local/mariadb
-DATADIR=/u01/mariadb
 BKDIR=/dbbackup
 
 ### PRE
 mkdir -p $BKDIR
 chown -R maria.maria $BKDIR
 mkdir -p /home/maria/bin
+chown -R maria.maria /home/maria/bin
 
 ### BACKUP
-echo "mariabackup --defaults-file=/etc/my.cnf --user=root --password='123\$qwer' --target-dir=/dbbackup --backup" > /home/maria/bin/1.backup.sh
-echo "mariabackup --defaults-file=/etc/my.cnf --user=root --password='123\$qwer' --target-dir=/dbbackup --prepare" > /home/maria/bin/2.prepare.sh
-echo 'cat /dbbackup/xtrabackup_checkpoints' > /home/maria/bin/3.check.sh
+echo -e 'mariabackup --defaults-file=/etc/my.cnf --user=root --password=123$qwer --target-dir=/dbbackup --backup' > /home/maria/bin/1.backup.sh
+echo -e 'mariabackup --defaults-file=/etc/my.cnf --user=root --password=123$qwer --target-dir=/dbbackup --prepare' > /home/maria/bin/2.prepare.sh
+echo -e 'cat /dbbackup/xtrabackup_checkpoints' > /home/maria/bin/3.check.sh
+
+### LAST
+echo build backup script complete.
+echo --- 
+echo "sudo su - maria -c 'ls bin'"
+sudo su - maria -c 'ls bin'
