b3a239937b036dc11984d65e2beafca27fb09aa7
taeyoungchoon <taeyoungchoon@gmail.com>
2020-08-25 16:47:11 +0900

    d
    Former-commit-id: 1ed47214fd262e18c7b6172a9092b70bb87749ce [formerly 7f5d2444315bbfe954991fec2df2450d6f8c0867] [formerly 4fdcb81c8fb87801a87f1166397b72fce4272c37]
    Former-commit-id: c2e46147a4998e83ef4b1428c2fbc4785827267f
    Former-commit-id: e884aceb222a261f3bde88d13f69e2ed502dfc7f
    Former-commit-id: 79eaadbf024206b5e0dd434d9942fc5a0f30a83d
    Former-commit-id: 444e3b38a5d1c38052de1f31b088bb8d7835e177

--- mariadb-go/install.sh
+++ mariadb-go/install.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-### if ONLINE
-# wget -O mariadb-10.2.32-linux-x86_64.tar.gz \
-# https://downloads.mariadb.org/interstitial/mariadb-10.2.32/bintar-linux-x86_64/mariadb-10.2.32-linux-x86_64.tar.gz
-
 ### ENV
 FILE=mariadb-10.2.32-linux-x86_64.tar.gz
 BASEDIRORIG=/usr/local/mariadb-10.2.32-linux-x86_64
