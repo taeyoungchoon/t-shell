00cdda0f967f0582910441146d495e8c056a0ac1
taeyoungchoon <taeyoungchoon@gmail.com>
2020-09-05 15:07:11 +0000

    d
    Former-commit-id: 58f88cfcb4494953c8d1864401fd9826b4d87c81 [formerly 8a5ee9d44b7f62350f8ce5a2bf545dd64beb2135] [formerly 8765ef8970fc608dfdfc97b556bd259e808324fc]
    Former-commit-id: 81e82303c05bccf9d814ba3dbe42f60daa320173
    Former-commit-id: 33955f207b537c3a9d9be8a68cbe9e53055e2236
    Former-commit-id: ee5d372905029dac882b2cbd35a74c040bc1bc8c
    Former-commit-id: 8ca9971b03790b7a3c4849cce91f148fe17d023b

--- github-clone.sh
+++ github-clone.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/taeyoungchoon/t-shell.git
 cd t-shell
-git config --global user.email ""
-git config --global user.name ""
+git config --global user.email "taeyoungchoon@gmail.com"
+git config --global user.name "taeyoungchoon"
 git config --global push.default simple
 git config --global credential.helper store
