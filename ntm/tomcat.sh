df0118683ac0f85080deda302baa75a601ac25c0
taeyoungchoon <taeyoungchoon@gmail.com>
2020-11-12 19:36:15 +0900

    d
    Former-commit-id: 6bc3e62d8000ef036c635f000a877d901ee772f4
    Former-commit-id: 6bcbe34bbc72155a5fc95b10732bfc25afd73e79

--- ntm/tomcat.sh
+++
@@ -1,7 +0,0 @@
-wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz
-tar xvfz apache-tomcat-9.0.14.tar.gz
-mv apache-tomcat-9.0.14 /opt
-cd /opt
-ln -s apache-tomcat-9.0.14 tomcat
-cd tomcat/bin
-./startup.sh
