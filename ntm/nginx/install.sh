6c2bf86d5532d2275718a77fa38c1818e67db7f1
taeyoungchoon <taeyoungchoon@gmail.com>
2020-11-13 11:57:54 +0000

    d
    Former-commit-id: d1c13aa1073e6bfeacd629438fe1f3c4d8f5e94f [formerly 6ee962d40a8b51e0666e99431a755515736560a8] [formerly db817b8c0501d09fa4a61ff85f58610946859cd0]
    Former-commit-id: f018d0273e7990db314293652bf80a74ad878ee7
    Former-commit-id: 0a83fd82dcc9b276a6a4de53c455951d5a194a48
    Former-commit-id: 0da5060d8898faf7ff2e4f92a6d4f362baafc261
    Former-commit-id: 8338e748992fc67f4066d40e206d56005be7874a

--- ntm/nginx/install.sh
+++
@@ -1,4 +0,0 @@
-wget http://nginx.org/packages/centos/7/x86_64/RPMS/nginx-1.18.0-1.el7.ngx.x86_64.rpm
-yum -y localinstall nginx-1.18.0-1.el7.ngx.x86_64.rpm 
-systemctl start nginx
-systemctl status nginx
