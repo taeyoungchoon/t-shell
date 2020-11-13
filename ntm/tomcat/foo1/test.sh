b3dc5edabb5dc1d0e3c753d15f49e8a4252b1410
taeyoungchoon <taeyoungchoon@gmail.com>
2020-11-13 05:49:14 +0000

    d
    Former-commit-id: 141d97bc4b4e41546b0bbc0031f589766482437d [formerly e0759eedad381bd50ce0e0936fdf4df55527b63a] [formerly fc54f53cba00775d2a26cbf377a6329f2a12d9d7]
    Former-commit-id: dec014e4d6bf564034ee0ce40a6a103555da5cad
    Former-commit-id: 6147ec5256b2c85652d1e733b541c94b3ea56fca
    Former-commit-id: 8e4d675c8e0422b0e09f851420d2114a6d96c4c0
    Former-commit-id: c9ef9f9c549002124f635ea6350f6c79a7333dc2

---
+++ ntm/tomcat/foo1/test.sh
@@ -0,0 +1,3 @@
+export CATALINA_HOME=/opt/tomcat
+export CATALINA_BASE=/opt/instance/foo1
+cp -a ../ROOT $CATALINA_BASE/webapps
---
+++ ntm/tomcat/foo2/test.sh
@@ -0,0 +1,3 @@
+export CATALINA_HOME=/opt/tomcat
+export CATALINA_BASE=/opt/instance/foo2
+cp -a ../ROOT $CATALINA_BASE/webapps
