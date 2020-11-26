037e7ce63d305c65994a7c5dfc8d56d5a2ae9251
taeyoungchoon <taeyoungchoon@gmail.com>
2020-11-26 06:43:01 +0000

    add some choco files
    Former-commit-id: 541f3266b2b0f5a52add5050a4895a6266b0f152
    Former-commit-id: d3ab929d7b7ed55123b070246fcb4644ba978814

---
+++ choco/install.bat
@@ -0,0 +1,27 @@
+choco install -f 7zip
+choco install -f adobereader
+choco install -f camstudio
+choco install -f clisp
+choco install -f dia
+choco install -f filezilla
+choco install -f ghostscript
+choco install -f gimp
+choco install -f git
+choco install -f googlechrome
+choco install -f firefox
+choco install -f heidisql
+choco install -f imageresizerapp
+choco install -f itunes
+choco install -f jre8
+choco install -f make
+# choco install -f --allow-empty-checksums nanumfont
+# choco install -y nanum-gothic-coding-font
+choco install -f vagrant
+choco install -f vim
+choco install -f virtualbox
+choco install -f vlc
+choco install -f vscode
+choco install -f windirstat
+choco install -f winscp
+choco install -f wireshark
+choco install -f xming
