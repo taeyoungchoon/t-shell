6f46b44ac02722144315dc157376bbf38a69b345
taeyoungchoon <taeyoungchoon@gmail.com>
2020-11-30 00:56:33 +0000

    d
    Former-commit-id: 854ac87a7d364b239ed5c63bb88d4a2399470ca3
    Former-commit-id: ec92d32b5998cf7f4b8176c434df7c773083700b

--- choco/install.bat
+++ choco/install.bat
@@ -1,28 +1,29 @@
-choco install -f 7zip
-choco install -f adobereader
-choco install -f camstudio
-choco install -f clisp
-choco install -f dia
-choco install -f freemind
-choco install -f filezilla
-choco install -f ghostscript
-choco install -f gimp
-choco install -f git
-choco install -f googlechrome
-choco install -f firefox
-choco install -f heidisql
-choco install -f imageresizerapp
-choco install -f itunes
-choco install -f jre8
-choco install -f make
-# choco install -f --allow-empty-checksums nanumfont
+choco install 7zip
+choco install adobereader
+choco install camstudio
+choco install clisp
+choco install dia
+choco install freemind
+choco install filezilla
+choco install ghostscript
+choco install gimp
+choco install git
+choco install googlechrome
+choco install firefox
+choco install heidisql
+choco install imageresizerapp
+choco install itunes
+choco install jre8
+choco install make
+# choco install --allow-empty-checksums nanumfont
 # choco install -y nanum-gothic-coding-font
-choco install -f vagrant
-choco install -f vim
-choco install -f virtualbox
-choco install -f vlc
-choco install -f vscode
-choco install -f windirstat
-choco install -f winscp
-choco install -f wireshark
-choco install -f xming
+choco install vagrant
+choco install vim
+choco install virtualbox
+choco install vlc
+choco install vscode
+choco install windirstat
+choco install winpcap
+choco install winscp
+choco install wireshark
+choco install xming
