1c3574aa2035e68e8a811e1a38e559cc83d59983
taeyoungchoon <taeyoungchoon@gmail.com>
2020-12-01 10:30:43 +0900

    d
    Former-commit-id: 8fea628f57ead55dcc986bee22b1330d7e511140 [formerly f363035e7d4721e26eca9c63a9065403216ca40b] [formerly 5574c63b5f371b7e9f2063c2ea1e7ba7a6693813]
    Former-commit-id: 82861c56bafc55bad96135bd48beaf062b39f2b4
    Former-commit-id: 7a8b48efa7be997e06582a33821b31bee6417296
    Former-commit-id: 7ccf49640d7609f2cacec580a06305abb17282c2
    Former-commit-id: d0a6da6b5c4f8539a026624d544c7a7551f8fc72

--- openstack/devstack/as-stack/go.sh
+++ openstack/devstack/as-stack/go.sh
@@ -1,5 +1,7 @@
-#git clone https://git.openstack.org/openstack-dev/devstack -b stable/rocky
-git clone https://git.openstack.org/openstack-dev/devstack -b stable/pike
+git clone https://git.openstack.org/openstack-dev/devstack -b stable/victoria
+# git clone https://git.openstack.org/openstack-dev/devstack -b stable/rocky
+# git clone https://git.openstack.org/openstack-dev/devstack -b stable/queens
+# git clone https://git.openstack.org/openstack-dev/devstack -b stable/pike
 cp local.conf devstack/
 cd devstack
 ./stack.sh
