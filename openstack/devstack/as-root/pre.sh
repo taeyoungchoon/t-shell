00acac393bc8c35061d9b239ab865da69d81c840
taeyoungchoon <taeyoungchoon@gmail.com>
2021-03-13 13:54:22 +0900

    d

--- openstack/devstack/as-root/pre.sh
+++ openstack/devstack/as-root/pre.sh
@@ -1,9 +1,9 @@
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+cp -a ../as-stack /opt/stack/
 chmod 755 /opt/stack
 chown -R stack.stack /opt/stack
 # one more thing before you go
 yum install systemd-devel libibverbs -y
 # fine
-cp -a ../as-stack /opt/stack/
 sudo su - stack
