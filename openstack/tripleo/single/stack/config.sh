3c22dce4c0ba0b216606127e7bdefcedaf99db69
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-01 07:38:24 +0100

    lots

---
+++ openstack/tripleo/single/stack/config.sh
@@ -0,0 +1,9 @@
+#
+# non-containerized
+#
+# cp /usr/share/instack-undercloud/undercloud.conf.sample ~/undercloud.conf
+
+#
+# containerized
+#
+cp /usr/share/python-tripleoclient/undercloud.conf.sample ~/undercloud.conf
--- openstack/tripleo/single/stack/pkg.sh
+++ openstack/tripleo/single/stack/pkg.sh
@@ -1,18 +1,4 @@
-# https://docs.openstack.org/project-deploy-guide/tripleo-docs/latest/deployment/install_undercloud.html
-
-sudo yum install -y wget
-sudo wget \
-https://trunk.rdoproject.org/centos7/current/python2-tripleo-repos-0.0.1-0.20200409224957.8bac392.el7.noarch.rpm
-sudo yum localinstall python2-tripleo-repos-0.0.1-0.20200409224957.8bac392.el7.noarch.rpm
-sudo yum install -y
-# sudo yum install -y https://trunk.rdoproject.org/centos7/current/python2-tripleo-repos-<version>.el7.centos.noarch.rpm
+sudo yum install -y https://trunk.rdoproject.org/centos7/current/python2-tripleo-repos-0.0.1-0.20200409224957.8bac392.el7.noarch.rpm
 sudo -E tripleo-repos -b queens current
-# sudo -E tripleo-repos -b queens current ceph
-sudo yum install -y python-requests
 sudo yum install -y python-tripleoclient
-# sudo -E tripleo-repos -b queens current ceph
-ls /usr/share/instack-undercloud/undercloud.conf.sample
-cp /usr/share/instack-undercloud/undercloud.conf.sample ~/undercloud.conf
-# openstack undercloud install
-# openstack undercloud install --verbose
-
+cp /usr/share/python-tripleoclient/undercloud.conf.sample undercloud.conf
