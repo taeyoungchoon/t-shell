53cf8757cc0b97eac0b7577f889ce1ea494f26c2
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-02 14:31:48 +0900

    d

--- openstack/tripleo/single/outside/bootstrap.sh
+++ openstack/tripleo/single/outside/bootstrap.sh
@@ -1,3 +1,7 @@
+# net.ipv6.conf.all.disable_ipv6=1
+# net.ipv6.conf.default.disable_ipv6=1
+# net.ipv6.conf.lo.disable_ipv6=1
+
 sudo yum install -y bash-completion psmisc git
 
 sudo timedatectl set-timezone Asia/Seoul
