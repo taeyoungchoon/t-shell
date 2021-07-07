a9a2afbdb6ed56092c5c54b59752f30c36cdea8b
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-07 08:00:47 +0100

    d

--- openstack/tripleo/pm/outside/bootstrap.sh
+++ openstack/tripleo/pm/outside/bootstrap.sh
@@ -7,7 +7,7 @@ sudo yum install -y bash-completion psmisc git
 sudo timedatectl set-timezone Asia/Seoul
 
 sudo su - root -c 'git clone https://github.com/taeyoungchoon/t-shell.git'
-sudo su - root -c 'cd /root/t-shell/openstack/tripleo/single/inside; sh user.sh'
+sudo su - root -c 'cd /root/t-shell/openstack/tripleo/pm/inside; sh user.sh'
 
 sudo su - stack -c 'git clone https://github.com/taeyoungchoon/t-shell.git'
-sudo su - stack -c 'ln -s /home/stack/t-shell/openstack/tripleo/single/stack .'
+sudo su - stack -c 'ln -s /home/stack/t-shell/openstack/tripleo/pm/stack .'
