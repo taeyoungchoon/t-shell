98a5bc951576da32951122df9b4c406412cee2fc
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-07 21:58:51 +0900

    d

--- openstack/tripleo/pm/stack/overcloud.sh
+++ openstack/tripleo/pm/stack/overcloud.sh
@@ -1,13 +1,18 @@
 ## image first
 # https://docs.openstack.org/project-deploy-guide/tripleo-docs/latest/deployment/install_overcloud.html
 # download images
-mkdir images
-https://images.rdoproject.org/centos7/queens/rdo_trunk/current-tripleo/ironic-python-agent.tar
-https://images.rdoproject.org/centos7/queens/rdo_trunk/current-tripleo/overcloud-full.tar
+wget https://images.rdoproject.org/centos7/queens/rdo_trunk/current-tripleo/ironic-python-agent.tar
+wget https://images.rdoproject.org/centos7/queens/rdo_trunk/current-tripleo/overcloud-full.tar
 # extract
+mkdir ~/images
+tar xf ironic-python-agent.tar -C images
+tar xf overcloud-full.tar -C images
 
 ## image upload
+cd images
+openstack overcloud image upload
 openstack image list
+
 ls -ltr /httpboot
 
 ## neutron
