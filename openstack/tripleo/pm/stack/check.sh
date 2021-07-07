66eefe4eb0c08c0e6d7798f8c4bca3ae9a126282
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-07 08:33:12 +0100

    d

--- openstack/tripleo/pm/stack/check.sh
+++ openstack/tripleo/pm/stack/check.sh
@@ -1,15 +1,18 @@
 source ~/stackrc
+
 openstack endpoint list
 openstack host list
 cat /etc/os-net-config/config.json
-python -m json.tool /etc/os-net-config/config.json
+# python -m json.tool /etc/os-net-config/config.json
+cat /etc/os-net-config/config.json | jq
 sudo ovs-vsctl show
 ls /etc/sysconfig/network-script/ifcfg-br-ctlplane
 cat /etc/sysconfig/network-script/ifcfg-br-ctlplane
-
 ls .instack/
 ironic node-list
 openstack baremetal node list
 ls /httpboot
-ls /etc/sysconfig/network-scripts/ifcfg-eth1
+ls /etc/sysconfig/network-scripts/ifcfg-enp3s0f1
+cat /etc/sysconfig/network-scripts/ifcfg-enp3s0f1
 ls /etc/ironic-inspector
+sudo cat /etc/ironic-inspector/dnsmasq.conf
