4905f69f27408dbd77a42f88986b05b5456f19c9
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-12 07:45:05 +0100

    remove dup code

--- openstack/tripleo/pm/outside/bootstrap.sh
+++ openstack/tripleo/pm/outside/bootstrap.sh
@@ -6,7 +6,7 @@ sudo yum install -y bash-completion psmisc git
 
 sudo timedatectl set-timezone Asia/Seoul
 
-sudo su - root -c 'git clone https://github.com/taeyoungchoon/t-shell.git'
+# sudo su - root -c 'git clone https://github.com/taeyoungchoon/t-shell.git'
 sudo su - root -c 'cd /root/t-shell/openstack/tripleo/pm/inside; sh user.sh'
 
 sudo su - stack -c 'git clone https://github.com/taeyoungchoon/t-shell.git'
