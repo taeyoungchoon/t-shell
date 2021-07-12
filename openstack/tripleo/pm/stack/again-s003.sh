ef5879a379499d1e04490c1d721d020019078fae
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-12 03:55:01 +0100

    d

--- openstack/tripleo/pm/stack/again-s003.sh
+++ openstack/tripleo/pm/stack/again-s003.sh
@@ -1,3 +1,5 @@
 openstack baremetal node delete s003
 openstack overcloud node import instackenv-s003.json
 openstack overcloud node introspect --all-manageable
+openstack overcloud node provide --all-manageable
+
--- openstack/tripleo/pm/stack/again.sh
+++ openstack/tripleo/pm/stack/again.sh
@@ -1,3 +1,5 @@
 openstack baremetal node delete s002
 openstack overcloud node import instackenv-s002.json
 openstack overcloud node introspect --all-manageable
+openstack overcloud node provide --all-manageable
+
