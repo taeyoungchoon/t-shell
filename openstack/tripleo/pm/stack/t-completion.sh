2bd22ba865375b4350fc41c878bd2d88e1af1eac
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-14 01:07:30 +0100

    d

--- openstack/tripleo/pm/stack/t-completion.sh
+++ openstack/tripleo/pm/stack/t-completion.sh
@@ -1,1 +1,2 @@
 openstack complete | sudo tee /etc/bash_completion.d/osc.bash_completion > /dev/null
+source /etc/bash_completion.d/osc.bash_completion
--- openstack/tripleo/pm/stack/t-pkg.sh
+++ openstack/tripleo/pm/stack/t-pkg.sh
@@ -1,3 +1,4 @@
+sudo yum install wget -y
 sudo yum install -y https://trunk.rdoproject.org/centos7/current/python2-tripleo-repos-0.0.1-0.20200409224957.8bac392.el7.noarch.rpm
 sudo -E tripleo-repos -b queens current
 sudo yum install -y python-tripleoclient
