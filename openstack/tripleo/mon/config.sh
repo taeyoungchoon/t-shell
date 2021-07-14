aba7ae4c529f70d408b1174ded9f6eb82d8ba402
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-14 08:27:39 +0100

    d

--- openstack/tripleo/mon/config.sh
+++ openstack/tripleo/mon/config.sh
@@ -1,5 +1,6 @@
 cd /usr/local/nagios/etc/
 mkdir servers printers switches routers
 sed -i 's/#cfg_dir=/cfg_dir=/' /usr/local/nagios/etc/nagios.cfg
+cd -
 cp cfg/* /usr/local/nagios/etc/servers/
 systemctl restart nagios
