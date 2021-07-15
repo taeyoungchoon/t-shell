beda7350ce7f719c240d0da4e536c60b73f66942
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-15 06:47:37 +0100

    d

---
+++ openstack/tripleo/shared/ntpd/firewall.sh
@@ -0,0 +1,7 @@
+service='ntp'
+if firewall-cmd --list-services | grep -w $service &>/dev/null; then
+        echo "$service port opened";
+else
+        firewall-cmd --permanent --add-service=$service
+        firewall-cmd --reload
+fi
--- openstack/tripleo/shared/ntpd/less-restricted.sh
+++ openstack/tripleo/shared/ntpd/less-restricted.sh
@@ -1,6 +1,14 @@
-if grep 'restrict 192.168.25.0 mask 255.255.255.0 nomodify notrap' /etc/ntp.conf &>/dev/null; then
+if ntpdc -c 'reslist -4' | grep -w 192.168.25.0 &>/dev/null; then
 	echo "192.168.25.0 are less restricted";
 else
 	sed -i '/#restrict 192.168.1.0 mask 255.255.255.0 nomodify notrap/ a \
 restrict 192.168.25.0 mask 255.255.255.0 nomodify notrap' /etc/ntp.conf;
 fi
+
+if ntpdc -c 'reslist -4' | grep -w 172.20.99.0 &>/dev/null; then
+	echo "172.20.99.0 are less restricted";
+else
+	sed -i '/#restrict 192.168.1.0 mask 255.255.255.0 nomodify notrap/ a \
+restrict 172.20.99.0 mask 255.255.255.0 nomodify notrap' /etc/ntp.conf;
+fi
+
