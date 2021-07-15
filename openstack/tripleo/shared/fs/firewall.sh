0f3a82e3eef3a228e14b044262d9db58339c9d6f
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-15 06:08:00 +0100

    d

---
+++ openstack/tripleo/shared/fs/firewall.sh
@@ -0,0 +1,7 @@
+service='http'
+if firewall-cmd --list-services | grep -w $service &>/dev/null; then
+        echo "$service port opened";
+else
+        firewall-cmd --permanent --add-service=$service
+        firewall-cmd --reload
+fi
