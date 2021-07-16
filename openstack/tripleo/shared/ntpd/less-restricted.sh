bf07804e95e5f1a948809f64a754db37a6d5f25f
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-16 01:37:37 +0100

    d

--- openstack/tripleo/shared/ntpd/less-restricted.sh
+++ openstack/tripleo/shared/ntpd/less-restricted.sh
@@ -1,11 +1,11 @@
-if ntpdc -c 'reslist -4' | grep -w 192.168.25.0 &>/dev/null; then
+if ntpdc -n -c 'reslist -4' | grep -w 192.168.25.0 &>/dev/null; then
 	echo "192.168.25.0 are less restricted";
 else
 	sed -i '/#restrict 192.168.1.0 mask 255.255.255.0 nomodify notrap/ a \
 restrict 192.168.25.0 mask 255.255.255.0 nomodify notrap' /etc/ntp.conf;
 fi
 
-if ntpdc -c 'reslist -4' | grep -w 172.20.99.0 &>/dev/null; then
+if ntpdc -n -c 'reslist -4' | grep -w 172.20.99.0 &>/dev/null; then
 	echo "172.20.99.0 are less restricted";
 else
 	sed -i '/#restrict 192.168.1.0 mask 255.255.255.0 nomodify notrap/ a \
