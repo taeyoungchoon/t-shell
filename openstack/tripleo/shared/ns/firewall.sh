41e2105c3c73de9e5b7b2671dd96cba895646eb5
taeyoungchoon <taeyoungchoon@gmail.com>
2021-07-16 02:27:13 +0100

    d

--- openstack/tripleo/shared/ns/firewall.sh
+++ openstack/tripleo/shared/ns/firewall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function activate {
+function activate_port {
 	local port=$1
 
 	if firewall-cmd --list-ports | grep -w $port &>/dev/null; then
@@ -11,6 +11,21 @@ function activate {
 	fi
 }
 
-for port in 53/tcp 53/udp; do
-	activate $port
+# for port in 53/tcp 53/udp; do
+# 	activate_port $port
+# done
+
+function activate_service {
+        local service=$1
+
+	if firewall-cmd --list-services | grep -w $service &>/dev/null; then
+		echo "$service service opened";
+	else
+		firewall-cmd --permanent --add-service=$service -q
+		firewall-cmd --reload -q
+	fi
+}
+
+for service in dns; do
+	activate_service $service
 done
