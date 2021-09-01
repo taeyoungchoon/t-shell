8fca4616620ed110e00945016a2a085e89094d83
taeyoungchoon <taeyoungchoon@gmail.com>
2021-09-02 08:07:24 +0900

    d

--- openstack/devstack/bootstrap.sh
+++ openstack/devstack/bootstrap.sh
@@ -8,9 +8,14 @@ systemctl reload sshd
 echo 'pkg: install essential packages'
 yum install -y git emacs-nox vim psmisc bash-completion strace procps-ng coreutils grep
 
-echo 'git: git clone t-shell'
-cd /root; git clone https://github.com/taeyoungchoon/t-shell.git
-
 echo 'man: rel'
 yum update -y pam
 yum reinstall -y shadow-utils sysvinit-tools
+
+echo 'git: git clone t-shell'
+cd /root; git clone https://github.com/taeyoungchoon/t-shell.git
+ln -s t-shell/openstack/devstack/as-root .
+
+# one more thing
+cd as-root; make
+
