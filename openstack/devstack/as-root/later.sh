558860238077e64d515042fa55d6eec8bc27e1f4
taeyoungchoon <taeyoungchoon@gmail.com>
2021-09-02 01:06:02 +0100

    d

--- openstack/devstack/as-root/later.sh
+++ openstack/devstack/as-root/later.sh
@@ -1,1 +1,2 @@
-sudo su - stack -c "cd as-stack; make; cd devstack; ./stack.sh; sleep 3; curl localhost/identity | jq"
+sudo su - stack -c "cd as-stack; make; cd devstack; ./stack.sh; \
+sleep 3; sudo yum -y install jq; curl localhost/identity | jq"
