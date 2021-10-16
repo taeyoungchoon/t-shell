7d69f51735467d50c7c9a5d1e2c7af580c98b1ae
taeyoungchoon <taeyoungchoon@gmail.com>
2021-10-16 22:43:38 +0900

    d

--- kvm/enable.sh
+++ kvm/enable.sh
@@ -1,5 +1,16 @@
-dnf install libvirt -y
+dnf -y install libvirt
 systemctl --no-pager status libvirtd
 systemctl enable libvirtd
 systemctl start libvirtd
 virt-host-validate
+
+# dnf -y install epel-release
+dnf -y install libguestfs-tools
+osinfo-query os
+virt-builder --note cirros0.4.0
+virt-builder fedora-27 --root-password password:123456
+virt-install \
+   --name f27vm1 --ram 1024 \
+   --disk path=fedora-27.img,format=raw \
+   --os-variant fedora27 \
+   --import
