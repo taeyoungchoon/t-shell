670f8ea14cc435223694b2e9a55b65bdd559db81
taeyoungchoon <taeyoungchoon@gmail.com>
2021-10-20 03:34:34 +0100

    d

---
+++ kvm/download-centos8.sh
@@ -0,0 +1,3 @@
+cd /var/lib/libvirt/images
+wget http://mirror.kakao.com/centos/8.4.2105/isos/x86_64/CentOS-8.4.2105-x86_64-dvd1.iso
+wget http://mirror.kakao.com/centos/8.4.2105/isos/x86_64/CentOS-8.4.2105-x86_64-boot.iso
