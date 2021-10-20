df469e70958e11e989315359ea2050d8b469bb2b
taeyoungchoon <taeyoungchoon@gmail.com>
2021-10-20 02:39:47 +0100

    d

--- kvm/cirros.sh
+++ kvm/cirros.sh
@@ -6,4 +6,4 @@ virsh vol-upload ci0.qcow2 cirros-0.4.0-x86_64-disk.qcow2 --pool images
 qemu-img info ci0.qcow2
 ip a s dev virbr0
 cat /var/lib/libvirt/dnsmasq/default.conf
-virt-install   --name ci0   --memory 512   --vcpus 1   --import   --disk=/var/lib/libvirt/images/ci0.qcow2,format=qcow2,bus=virtio --network network=default --graphics vnc,keymap=local --noautoconsole
+virt-install --name ci0 --memory 512 --vcpus 1 --import --disk=/var/lib/libvirt/images/ci0.qcow2,format=qcow2,bus=virtio --network network=default --graphics vnc,keymap=local --noautoconsole
