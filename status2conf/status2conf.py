a4fc3e399890ab2eae645953b11beb6179cd484b
taeyoungchoon <taeyoungchoon@gmail.com>
2021-10-22 01:14:57 +0100

    d

---
+++ status2conf/status2conf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# status to conf
+#
+# 1. copy port, name, status, vlan field values to notepad from excel
+# 2. save data as input.tsv
+# 3. run this script
+#
+
+import csv
+
+with open("input.tsv") as data:
+        for tokens in csv.reader(data, dialect="excel-tab"):
+                interface_value, description_value, status_value, vlan_value = tokens
+                print(f'!')
+                print(f'interface {interface_value}')
+                print(f'description "{description_value}"')
+                print(f'switch access vlan {vlan_value}')
+
+print(f'!')
