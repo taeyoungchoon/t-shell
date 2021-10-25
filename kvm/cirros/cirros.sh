345ce96b2e74d3a1314cfea4b5bde0dfdf8ecfde
taeyoungchoon <taeyoungchoon@gmail.com>
2021-10-25 06:59:42 +0100

    d

--- kvm/cirros/cirros.sh
+++ kvm/cirros/cirros.sh
@@ -1,8 +1,10 @@
 virsh pool-list
-virsh vol-list images
-virsh vol-create-as images ci0.qcow2 128M --format qcow2
-virsh vol-upload ci0.qcow2 cirros-0.4.0-x86_64-disk.qcow2 --pool images
+virsh vol-list default
+
+virsh vol-create-as default ci0.qcow2 128M --format qcow2
+virsh vol-upload ci0.qcow2 cirros-0.4.0-x86_64-disk.qcow2 --pool default
 qemu-img info ci0.qcow2
+
 ip a s dev virbr0
 cat /var/lib/libvirt/dnsmasq/default.conf
 
