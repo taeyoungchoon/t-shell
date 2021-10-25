934ac871ed1ee47391d51ae9e25e8179098d486e
taeyoungchoon <taeyoungchoon@gmail.com>
2021-10-25 06:57:50 +0100

    d

---
+++ kvm/cirros/cirros-download.sh
@@ -0,0 +1,3 @@
+wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
+mv cirros-0.4.0-x86_64-disk.img cirros-0.4.0-x86_64-disk.qcow2
+qemu-img info cirros-0.4.0-x86_64-disk.qcow2
