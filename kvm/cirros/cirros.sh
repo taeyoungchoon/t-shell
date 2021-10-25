2705bb109274935f0b317a4a8f8713e49dda1aa3
taeyoungchoon <taeyoungchoon@gmail.com>
2021-10-25 07:05:26 +0100

    d

--- kvm/cirros/cirros.sh
+++ kvm/cirros/cirros.sh
@@ -8,4 +8,4 @@ qemu-img info ci0.qcow2
 ip a s dev virbr0
 cat /var/lib/libvirt/dnsmasq/default.conf
 
-virt-install --name ci0 --memory 512 --vcpus 1 --import --disk=/var/lib/libvirt/images/ci0.qcow2,format=qcow2,bus=virtio --network network=default --graphics vnc,keymap=local --noautoconsole
+virt-install --name ci0 --memory 512 --vcpus 1 --import --os-variant cirros0.4.0 --disk=/var/lib/libvirt/images/ci0.qcow2,format=qcow2,bus=virtio --network network=default --graphics vnc,keymap=local --noautoconsole
