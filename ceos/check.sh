88de29d2b4889162ff02b686175734c18eb332b9
taeyoungchoon <taeyoungchoon@gmail.com>
2021-11-05 13:56:46 +0100

    d

--- ceos/check.sh
+++ ceos/check.sh
@@ -1,3 +1,19 @@
+printf "\n"
+printf "network list\n"
+printf "============\n"
 docker network list
+
+printf "\n"
+printf "image list\n"
+printf "==========\n"
 docker image list
+
+printf "\n"
+printf "container list\n"
+printf "==============\n"
 docker container ls -a
+
+printf "\n"
+printf "container logs\n"
+printf "==============\n"
+docker container ls -q | sed 's/^/docker container logs /' | sh | grep "User System"
