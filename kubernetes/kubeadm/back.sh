f1e408863bbc904a697a3b5ccc1bd657a2c2942f
taeyoungchoon <taeyoungchoon@gmail.com>
2021-12-15 13:47:43 +0000

    d

---
+++ kubernetes/kubeadm/back.sh
@@ -0,0 +1,5 @@
+make -f makefile-for-m check
+make -f makefile-for-m stop
+make -f makefile-for-m check
+make -f makefile-for-m clean
+make -f makefile-for-m check
---
+++ kubernetes/kubeadm/go.sh
@@ -0,0 +1,3 @@
+make -f makefile-for-m check
+make -f makefile-for-m start
+make -f makefile-for-m check
