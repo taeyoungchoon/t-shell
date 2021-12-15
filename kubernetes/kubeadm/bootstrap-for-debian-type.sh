23ed7b41c52d5f9452dc140fbb851738c223f689
taeyoungchoon <taeyoungchoon@gmail.com>
2021-12-15 13:09:36 +0000

    d

--- kubernetes/kubeadm/bootstrap-for-debian-type.sh
+++ kubernetes/kubeadm/bootstrap-for-debian-type.sh
@@ -1,1 +1,8 @@
 apt install git -y
+id
+cd /root
+git clone https://github.com/taeyoungchoon/t-shell.git
+cd ~/t-shell/kubernetes/kubeadm
+pwd
+sh os.sh
+sh pre-docker.sh
--- kubernetes/kubeadm/the-k.sh
+++ kubernetes/kubeadm/the-k.sh
@@ -1,9 +1,25 @@
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl --system
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+https://github.com/flannel-io/flannel#flannel
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+kubectl run n1 --image nginx
+kubectl get all
