08f1805f402f1fc9d7cfe47890fde01bef3a0cf4
taeyoungchoon <taeyoungchoon@gmail.com>
2021-12-16 09:47:17 +0900

    d

--- kubernetes/kubeadm/test.sh
+++ kubernetes/kubeadm/test.sh
@@ -2,11 +2,14 @@ export KUBECONFIG=/etc/kubernetes/admin.conf
 
 kubectl get all -n kube-system
 kubectl run n1 --image nginx
+kubectl get all
 sleep 60
 kubectl get all -n kube-system
 kubectl run n2 --image nginx
+kubectl get all
 sleep 60
 kubectl get all -n kube-system
 kubectl run n3 --image nginx
+kubectl get all
 
 echo done
