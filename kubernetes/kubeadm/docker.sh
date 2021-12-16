bad73579a1b1c30617913542d092f0c10b02064f
taeyoungchoon <taeyoungchoon@gmail.com>
2021-12-16 10:59:10 +0900

    d

--- kubernetes/kubeadm/docker.sh
+++ kubernetes/kubeadm/docker.sh
@@ -1,5 +1,5 @@
 sudo apt-get -qq remove docker docker-engine docker.io containerd runc 
-sudo apt-get -qq udpate
+sudo apt-get -qq update
 sudo apt-get -qq install ca-certificates curl gnupg lsb-release &>/dev/null
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
