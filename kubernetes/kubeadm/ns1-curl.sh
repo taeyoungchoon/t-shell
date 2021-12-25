05e7af40c289bea1aee8381a907b17d2c8a386c5
taeyoungchoon <taeyoungchoon@gmail.com>
2021-12-25 23:30:50 +0900

    d

--- kubernetes/kubeadm/ns1-curl.sh
+++ kubernetes/kubeadm/ns1-curl.sh
@@ -1,6 +1,5 @@
 kubectl exec pod/n1 -- apt-get update -qq
 kubectl exec pod/n1 -- apt-get install jq -qq
 kubectl exec pod/n1 -- apt-get install iproute2 -qq
-#kubectl exec pod/n1 -- curl -s localhost
-IP=`ip -j a | jq -r '.[] | select(.ifname == "eth0") | .addr_info[].local'`
-kubectl exec pod/n1 -- curl -s $IP
+kubectl cp curl.sh n1:/
+kubectl exec pod/n1 -- sh curl.sh
