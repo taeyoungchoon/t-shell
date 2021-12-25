71c2c2b372cb3e033dfa9aeb47e2d418bb11f63f
taeyoungchoon <taeyoungchoon@gmail.com>
2021-12-25 22:13:13 +0900

    d

--- kubernetes/kubeadm/ns1-ip.sh
+++ kubernetes/kubeadm/ns1-ip.sh
@@ -1,6 +1,9 @@
 kubectl exec pod/n1 -- apt-get update -y
 kubectl exec pod/n1 -- apt-get install iproute2 -y
+echo '-- host'
 ip -4 -br a
 ip r
+bridge link
+echo '-- pod/n1'
 kubectl exec pod/n1 -- ip -4 -br a
 kubectl exec pod/n1 -- ip r
