ddfeff6a2161c840127c66caeef868404419683e
taeyoungchoon <taeyoungchoon@gmail.com>
2021-12-25 21:01:23 +0900

    add skel for kubernetes dashboard

--- kubernetes/kubeadm/check.sh
+++ kubernetes/kubeadm/check.sh
@@ -1,12 +1,15 @@
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-echo kubectl get sa ------------------------
+echo --- kubectl get sa
 kubectl get sa
 
-echo kubectl get all -n kube-system --------
+echo --- kubectl get all -n kube-system 
 kubectl get all -n kube-system
 
-echo kubectl get all -----------------------
+echo --- kubectl get all 
 kubectl get all
+
+echo --- kubectl get all -n kubernetes-dashboard
+kubectl get all -n kubernetes-dashboard
 
 echo done
---
+++ kubernetes/kubeadm/dashboard.sh
@@ -0,0 +1,16 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
+# kubectl apply -f dashboard-adminuser.yaml
+# kubectl get sa -n kubernetes-dashboard
+# kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
+# kubectl proxy --port=8001 --address="192.168.56.100" --accept-hosts="^*$" & 
+#
+# http://192.168.56.100:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login
+# kubectl edit service kubernetes-dashboard -n kubernetes-dashboard
+# NodePort
+# kubectl -n kubernetes-dashboard delete serviceaccount admin-user
+# kubectl -n kubernetes-dashboard delete clusterrolebinding admin-user
+#
+# https://journes.tistory.com/43
+# https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
+# https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+
