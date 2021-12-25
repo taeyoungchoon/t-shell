a539e8d0c8f6ec66b591c8e570695d225dbe99d8
taeyoungchoon <taeyoungchoon@gmail.com>
2021-12-25 22:33:28 +0900

    d

--- kubernetes/kubeadm/ns1-curl.sh
+++ kubernetes/kubeadm/ns1-curl.sh
@@ -1,2 +1,5 @@
 kubectl exec pod/n1 -- apt-get update -qq
-kubectl exec pod/n1 -- curl -s localhost
+kubectl exec pod/n1 -- apt-get install jq -qq
+#kubectl exec pod/n1 -- curl -s localhost
+IP=`ip -j a | jq -r '.[] | select(.ifname == "eth0") | .addr_info[].local'`
+kubectl exec pod/n1 -- curl -s $IP
