5aa40ff8bf5002d4974ec087acc53fe437872461
taeyoungchoon <taeyoungchoon@gmail.com>
2022-01-10 20:39:14 +0900

    d

--- kubernetes/kubeadm/curl.sh
+++ kubernetes/kubeadm/curl.sh
@@ -2,4 +2,5 @@
 IP=$(ip -o -4 -j address show eth0 | jq -r '.[] | .addr_info[].local')
 echo ip: $IP
 echo curl -s $IP
-curl -s $IP
+curl -s -i $IP | grep 'Length'
+curl -s $IP | egrep 'title.*Welcome to nginx'
