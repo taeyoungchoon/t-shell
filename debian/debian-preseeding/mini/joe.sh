fd15eedd2dc51273ea4d53752967da7bb0bbf0bc
taeyoungchoon <taeyoungchoon@gmail.com>
2022-03-18 19:58:34 +0900

    d

--- debian/debian-preseeding/mini/joe.sh
+++ debian/debian-preseeding/mini/joe.sh
@@ -1,7 +1,10 @@
-virt-install --virt-type kvm --name fai0 \
---location /var/lib/libvirt/images/d.iso \
---os-variant debian11 \
---disk size=20 --memory 2000 \
---graphics none \
---console pty,target_type=serial \
---extra-args "console=ttyS0"
+virt-install --virt-type kvm \
+	     --name joe \
+	     --location /var/lib/libvirt/images/d.iso \
+	     --os-variant debian11 \
+	     --disk size=20 --memory 2000 \
+	     --graphics none \
+	     --autoconsole none \
+	     --initrd-inject=preseed.cfg \
+	     --console pty,target_type=serial \
+	     --extra-args "console=ttyS0"
