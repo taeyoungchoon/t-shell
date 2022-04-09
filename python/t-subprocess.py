3404373b1fa21a868b37126935d93de922939934
taeyoungchoon <taeyoungchoon@gmail.com>
2022-04-10 02:07:25 +0900

    d

--- python/t-subprocess.py
+++ python/t-subprocess.py
@@ -1,19 +1,29 @@
 import subprocess
-import re
+import subprocess as sp
 
-# proc = subprocess.Popen(['uname', '-a'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# (out, err) = proc.communicate()
-# hn, ver = out.split()[1:3]
-# print('\nhostname: %s\nversion: %s' % (hn.decode(), ver.decode()))
 
-proc = subprocess.run(['df', '-Pk', '-l'], capture_output=True)
+def fact():
+    '''gather fact'''
+    proc = subprocess.Popen(['uname', '-a'], stdout=sp.PIPE, stderr=sp.PIPE)
+    (out, err) = proc.communicate()
+    hn, ver = out.split()[1:3]
+    print('\nhostname: %s\nversion: %s' % (hn.decode(), ver.decode()))
 
-diskusage = proc.stdout.decode().splitlines()
-diskusage.pop(0)
+    
+def df():
+    '''df helper'''
+    proc = subprocess.run(['df', '-Pk', '-l'], capture_output=True)
 
-for line in diskusage:
-    filesystem, blocks, used, avaiable, capacity, mounted = line.split()
-    capacity = capacity.replace('%','')
-    capacity = int(capacity)
-    if capacity > 70:
-        print('Warning: {}% used on {}'.format(capacity, mounted))
+    diskusage = proc.stdout.decode().splitlines()
+    diskusage.pop(0)
+
+    for line in diskusage:
+        filesystem, blocks, used, avaiable, capacity, mounted = line.split()
+        capacity = capacity.replace('%', '')
+        capacity = int(capacity)
+        if capacity > 70:
+            print('Warning: {}% used on {}'.format(capacity, mounted))
+
+
+fact()
+df()
