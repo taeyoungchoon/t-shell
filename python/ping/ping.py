7de8ca282c7d615f995288b14ed154348ea6b308
taeyoungchoon <taeyoungchoon@gmail.com>
2022-04-14 21:14:18 +0900

    d

--- python/ping/ping.py
+++ python/ping/ping.py
@@ -1,6 +1,11 @@
 import subprocess
 import re
 
+'''
+run
+filter
+output
+'''
 
 def check_returncode(process):
     if process.returncode == 0:
@@ -8,12 +13,23 @@ def check_returncode(process):
     else:
         return False
 
+def run():
+    return subprocess.run(['ping', '-c', '3', '8.8.8.8'], capture_output=True)
 
-proc = subprocess.run(['ping', '-c', '3', '8.8.8.8'], capture_output=True)
-
-
-if check_returncode(proc):
+def filtering():
     status = proc.stdout.decode().splitlines().pop(-2)
     pattern = re.compile('(\d+.\d+)%')
     matched = pattern.search(status)
-    matched.group(1)
+    result = matched.group(1)
+    result = float(output)
+    return result
+
+output = 0
+
+if check_returncode(proc):
+    output = filtering()
+    if output == 0.0:
+        print(True)
+    else:
+        print(False)
+
