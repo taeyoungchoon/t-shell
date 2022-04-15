04e0c76309ce48832dd891a39dcc2cba406385e6
taeyoungchoon <taeyoungchoon@gmail.com>
2022-04-15 02:11:24 +0100

    working

--- python/ping/alive.py
+++ python/ping/alive.py
@@ -6,8 +6,6 @@ CMD = 'ping'
 OPTION = ''
 VARIABLE = '3'
 TARGET = '8.8.8.8'
-REQUIRED = 0
-
 
 def command_line():
     if sys.platform == 'win32':
@@ -17,36 +15,30 @@ def command_line():
 
     return CMD, OPTION, VARIABLE, TARGET
 
+def filter_loss(line):
+    pattern = re.compile('((\d+.\d+)|(\d+))%')
+    matched = pattern.search(line)
+    output = matched.group(1)
+    return float(output)
 
-def pinging():
+def pinging_loss():
     try:
         cmd = command_line()
-        print(cmd)
         proc = subprocess.run(cmd, capture_output=True, check=True)
     except subprocess.CalledProcessError as e:
         print(e)
         return
-
-    print(REQUIRED)
 
     if sys.platform == 'win32':
         REQUIRED = -3
     else:
         REQUIRED = -2
 
-    print(REQUIRED)
-
-    REQUIRED = -3
     status_line = proc.stdout.decode().splitlines().pop(REQUIRED)
-    print(proc.stdout.decode())
-
-    pattern = re.compile('((\d+.\d+)|(\d+))%')
-    matched = pattern.search(status_line)
-    output = matched.group(1)
-    return float(output)
+    return filter_loss(status_line)
 
 def main():
-    if pinging() == 0:
+    if pinging_loss() == 0:
         print(True)
 
 if __name__ == '__main__':
