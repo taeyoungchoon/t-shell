5ea3a3d664739fe75263d659904b322232b617ec
taeyoungchoon <taeyoungchoon@gmail.com>
2022-05-20 02:58:00 +0100

    d

--- ansible/workspace/p1.py
+++ ansible/workspace/p1.py
@@ -1,4 +1,4 @@
 import pyeapi
 
 conn = pyeapi.connect(host='192.168.25.251', transport='http', username='cvpadmin', password='1234qwer')
-conn.execute(['show verson'])
+conn.execute(['show version'])
