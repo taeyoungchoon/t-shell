8e8b7622ba8786048b8c4ef93f9aa7ed2508c59a
taeyoungchoon <taeyoungchoon@gmail.com>
2022-06-23 06:40:09 +0100

    add test code

--- ansible/workspace/p1.py
+++ ansible/workspace/p1.py
@@ -1,4 +1,4 @@
 import pyeapi
 
 conn = pyeapi.connect(host='192.168.25.251', transport='http', username='cvpadmin', password='1234qwer')
-conn.execute(['show version'])
+print(conn.execute(['show version']))
