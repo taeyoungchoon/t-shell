30560bdd4a9a91b107aee4ca7ae5d5c497dc2dfb
taeyoungchoon <taeyoungchoon@gmail.com>
2022-09-10 01:07:43 +0900

    d

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-yum -y -q install emacs-nox git psmisc tree bash-completion
+yum -y -q install emacs-nox git psmisc tree bash-completion bridge-utils libvirt-client libvirt wget
 
 grep "PasswordAuthentication no" /etc/ssh/sshd_config >/dev/null 2>/dev/null
 if test $? -eq 0; then
