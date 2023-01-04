0e268ce6cbd5cd32b2fc06125c5a95daabeeb716
taeyoungchoon <taeyoungchoon@gmail.com>
2023-01-04 01:27:18 +0000

    d

--- nic/ip.sh
+++ nic/ip.sh
@@ -12,9 +12,10 @@ function go {
 
 	# https://unix.stackexchange.com/questions/523020/differance-between-ip4-and-ipv4-addresses-nmcli
 	echo nmcli con mod $interface ip4 ${!ip}/${!nm} gw4 ${!gw} ipv4.method manual
-	echo nmcli con mod $interface ip4 ${!ip}/${!nm} gw4 ${!gw}	
+	# echo nmcli con mod $interface ip4 "${!ns}"
+
 	for s in ${!ns}; do
-	    echo nmcli con mod $interface ipv4.dns +$s
+	    echo nmcli con mod $interface +ipv4.dns $s
 	done
 
     done
