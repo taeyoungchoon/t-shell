0dd153bee6fa0610e1206c3752d69b199f85b661
taeyoungchoon <taeyoungchoon@gmail.com>
2023-02-07 05:11:59 +0000

    d

--- nic/using-awk/check.sh
+++ nic/using-awk/check.sh
@@ -1,7 +1,5 @@
 nmcli connection
 nmcli device
-[[ -e /proc/net/bonding/service ]] && cat /proc/net/bonding/service | egrep "Slave|Mode"  
-[[ -e /proc/net/bonding/rac ]] && cat /proc/net/bonding/rac | egrep "Slave|Mode"
-[[ -e /proc/net/bonding/nas ]] && cat /proc/net/bonding/nas | egrep "Slave|Mode"
+name=service; [[ -e /proc/net/bonding/$name ]] && echo name && cat /proc/net/bonding/service | egrep "Slave I|Mode"  
 ip -br -4 a
 route -n
