f390526b6faaae93dad26110bee17e2311faf9ec
taeyoungchoon <taeyoungchoon@gmail.com>
2023-02-13 21:37:59 +0900

    d

--- nic/using-awk/check.sh
+++ nic/using-awk/check.sh
@@ -1,6 +1,14 @@
-nmcli connection
-nmcli device
-name=service; [[ -e /proc/net/bonding/$name ]] && echo $name && cat /proc/net/bonding/service | egrep "Slave I|Mode"  
-ip -br -4 a
-route -n
-ip a | awk '/^[0-9]/ { print $2, $4, $5 }'
+#!/usr/bin/env bash
+
+if which -s nmcli; then 
+
+    nmcli connection
+    nmcli device
+    name=service; [[ -e /proc/net/bonding/$name ]] && echo $name && cat /proc/net/bonding/service | egrep "Slave I|Mode"  
+    ip -br -4 a
+    route -n
+    ip a | awk '/^[0-9]/ { print $2, $4, $5 }'
+
+else
+    echo nmcli required
+fi
--- nic/using-awk/clean.sh
+++ nic/using-awk/clean.sh
@@ -1,6 +1,12 @@
-# nmcli --terse connection show | cut -d : -f 1 | \
-#   while read name; do echo nmcli connection delete "$name"; done
-nmcli -t -f name c | \
-while read name; do 
-	echo nmcli connection delete "$name"; 
-done
+#!/usr/bin/env bash
+
+if which -s nmcli; then 
+    
+    nmcli -t -f name c | \
+	while read name; do 
+	    echo nmcli connection delete "$name"; 
+	done
+
+else
+    echo nmcli required
+fi
