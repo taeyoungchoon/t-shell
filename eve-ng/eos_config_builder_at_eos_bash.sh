d884353c5325391e3458fabe6a228e3ea1a6ac8b
taeyoungchoon <taeyoungchoon@gmail.com>
2023-08-24 05:17:01 +0100

    d

---
+++ eve-ng/eos_config_builder_at_eos_bash.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ip=$1
+
+if [[ -z $ip ]]; then
+        exit;
+fi
+
+hn=v${ip##*.}
+
+mac_old=$( Cli -c 'show interface management 1 | json' | awk -F'"' '/physicalAdd/ { print $4 }' )
+mac_new="50:03$(printf $mac_old | cut -b 6-18)"
+
+template=$(cat <<EOF
+conf t
+hostname HOSTNAME
+clock timezone Asia/Seoul
+ntp server 192.168.25.21
+username admin secret 1234qwer
+username mon secret 1234qwer
+enable password 1234qwer
+interface management 1
+ip address IP/24
+mac-address MAC
+exit
+ip route 0.0.0.0/0 192.168.25.1
+exit
+wr
+EOF
+)
+
+echo "$template" | sed -e "s/HOSTNAME/$hn/" -e "s/IP/$ip/" -e "s/MAC/$mac_new/"
