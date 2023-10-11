7d2b3e4e77651b0f0fbadc37d168eaa72151f501
taeyoungchoon <taeyoungchoon@gmail.com>
2023-10-11 02:38:20 +0100

    d

--- dns/ha-with-chroot/master/adding.sh
+++ dns/ha-with-chroot/master/adding.sh
@@ -1,7 +1,7 @@
 awk -i inplace '/soa/ { $6++; print }; ! /soa/ { print }' /var/named/t.com.db
 hostname=t$RANDOM
 ip=$(( ( RANDOM % 10 ) +1 )).$(( ( RANDOM % 10 ) +1 )).$(( ( RANDOM % 10 ) +1 )).$(( ( RANDOM % 10 ) +1 ))
-printf "%s\tA\t%s" $hostname $ip >> /var/named/t.com.db
+printf "%s\tA\t%s\n" $hostname $ip >> /var/named/t.com.db
 rndc reload
 sleep 0.5
 host $hostname.t.com 172.20.0.102
