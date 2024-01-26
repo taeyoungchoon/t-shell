f739c99b63302cf8f3c2850b76a7ada888db77d9
taeyoungchoon <taeyoungchoon@gmail.com>
2024-01-26 07:08:04 +0000

    d

--- mako/shut/shut.py
+++ mako/shut/shut.py
@@ -1,6 +1,4 @@
 from mako.template import Template
 
 tmp = Template(filename='shut.tmpl')
-#tmp = Template("hello, ${name}")
 print(tmp.render(intf=["en1","en2","en3"]))
-#print(tmp.render(name="jack"))
